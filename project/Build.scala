0635acc82a9b2eebd194425074c2fb8f6a7cab8b
namproctin <namproctin@gmail.com>
2013-09-06 12:32:06 +0700

    new change

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"	
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
